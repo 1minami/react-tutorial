432a6dbf30fb72d69c6918370983688bf4c0d522
takai <63587321+1minami@users.noreply.github.com>
2022-01-15 12:47:24 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
+import { ColorfulMeaage } from "./components/ColorfulMeaage";
+
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <ColorfulMeaage color="blue">お元気ですか？</ColorfulMeaage>
+      <ColorfulMeaage color="pink">元気です</ColorfulMeaage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>＼(^o^)／</p>}
+    </>
+  );
+};
+
+export default App;
